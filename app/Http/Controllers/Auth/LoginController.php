519bec3d3db684fae7fdcdc418ce6cd4d0ec5058
chitruong22 <chitruong2k1@gmail.com>
2022-08-12 11:24:36 +0700

    create file loginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
-
+// loginController
 class LoginController extends Controller
 {
     /*
