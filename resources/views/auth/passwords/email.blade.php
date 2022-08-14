2f207379552be9c2b5510efdf1c906dc4225140c
chitruong22 <chitruong2k1@gmail.com>
2022-08-14 14:00:04 +0700

    create verify.blade.php ,email .blade.php ,reset.blade.php ,in folder view views/admin/auth

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+// create email.blade.php in folder view views/admin/auth/passworld
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
